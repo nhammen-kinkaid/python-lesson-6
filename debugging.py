5489627c4960115287ff8bc5d1dc9cb941225e36
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-17 15:57:44 +0000

    Create a debugging example

---
+++ debugging.py
@@ -0,0 +1,28 @@
+# debugging example courtesy of LaunchCode education
+
+launchReady = False;
+fuelLevel = 17000;
+
+if fuel_Level >= 20000:
+   print('Fuel level cleared.');
+   launchReady = True
+else:
+   print('WARNING: Insufficient fuel!');
+   launchReady = False
+
+crewStatus = true
+computerStatus = 'green'
+
+if crewStatus and computerStatus == 'green':
+   print('Crew & computer cleared.')
+   launchReady = True
+else:
+   print('WARNING: Crew or computer not ready!')
+   launchReady = False
+
+if (launchReady):
+   print(("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...")
+   print("Ignition...")
+   print("Liftoff!")
+else:
+   print("Launch scrubbed.")
