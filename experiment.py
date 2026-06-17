8492b03f438e7cb2fde767de2c1d6923eab7e982
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-17 07:57:45 -0500

    Create experiment.py

--- experiment.py
+++ experiment.py
@@ -7,5 +7,11 @@ window = turtle.Screen()
 # Create a visible turtle object
 my_turtle = turtle.Turtle()
 
+def click_handler(x, y):
+  my_turtle.forward(100)
+  my_turtle.forward(-100)
+
+my_turtle.onclick(click_handler)
+
 # Keep the window open and listening for events
 window.mainloop()
