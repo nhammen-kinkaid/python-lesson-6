d15a7ab89a4dd745afaeaaebe11c01250c704456
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-17 15:10:53 +0000

    Create pinwheel.py

---
+++ pinwheel.py
@@ -0,0 +1,23 @@
+import turtle
+
+# Set up the screen window
+window = turtle.Screen()
+
+# Create a visible turtle object
+my_turtle = turtle.Turtle()
+my_turtle.shapesize(3)
+
+def click_handler(x, y):
+  my_turtle.clear()
+  my_turtle.speed(0)
+  # fastest speed
+
+  my_turtle.pendown()
+  my_turtle.forward(100)
+  my_turtle.forward(-100)
+  my_turtle.penup()
+
+my_turtle.onclick(click_handler)
+
+# Keep the window open and listening for events
+window.mainloop()
