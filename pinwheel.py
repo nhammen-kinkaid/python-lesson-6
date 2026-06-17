ad9a853799648ec9578af09483a50ac0681d0ea5
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-17 15:36:03 +0000

    pinwheel function

--- pinwheel.py
+++ pinwheel.py
@@ -7,14 +7,19 @@ window = turtle.Screen()
 my_turtle = turtle.Turtle()
 my_turtle.shapesize(3)
 
+def pinwheel(sides, size=100, backup=0):
+  for _ in range(sides):
+    my_turtle.forward(size)
+    my_turtle.forward(-backup)
+    my_turtle.right(360/sides)
+
 def click_handler(x, y):
   my_turtle.clear()
   my_turtle.speed(0)
   # fastest speed
 
   my_turtle.pendown()
-  my_turtle.forward(100)
-  my_turtle.forward(-100)
+  pinwheel(7)
   my_turtle.penup()
 
 my_turtle.onclick(click_handler)
